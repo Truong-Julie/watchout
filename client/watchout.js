f9e938f9971b76158fbdb3e06c66a6833f33f465
Truong-Julie <gamerlazer@gmail.com>
2016-08-06 10:20:01 -0700

    Remove extra d3 selectors from enemies

--- client/watchout.js
+++ client/watchout.js
@@ -42,25 +42,45 @@ var Enemy = function(id) {
   this.id = id;
   this.x = Math.random() * gameOptions.width;
   this.y = Math.random() * gameOptions.height;
-  // this.x = axes.x(Math.random()) * gameOptions.width;
-  // this.y = axes.y(Math.random()) * gameOptions.height;
 };
-// init a bunch of enenmies
+
+var Player = function () {
+  this.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+  this.fill = '#ff6600';
+  this.x = gameOptions.width * 0.5;
+  this.y = gameOptions.height * 0.5;
+  this.angle = 0;
+  this.r = 5;
+};
+
+
+
+/****************** ADD PLAYER TO THE BOARD ***********************/
+
+var mouse = new Player ();
+
+d3.selectAll(".mouse");
+
+  
+
+
+/****************** ADD ENEMIES TO THE BOARD **********************/
+
 var enemiesArray = [];
 for (var i = 0; i < gameOptions.nEnemies; i++) {
   var e = new Enemy(i);
   enemiesArray.push(e);
 }
-// push to enemies array
-
 
 // use d3 to add enemies as svg circles to the dom
-d3.select("body").select(".board").select("svg").selectAll("circle")
+d3.select(".enemy").selectAll("circle")
   .data(enemiesArray, function(d) { return d.id; })
   .enter()
   .append("circle")
   .attr('cx', function(d) { return d.x;} )
   .attr('cy', function(d) { return d.y;} );
+
+/******************* BOUNCING ENEMIES ******************************/
 
 function updatePosition(enemiesArray) {
   d3.selectAll("circle")
@@ -71,31 +91,9 @@ function updatePosition(enemiesArray) {
     .attr('cy', function(d) { return d.y = Math.random() * gameOptions.height; });
 }
 
-
 setInterval(function () {
   updatePosition(enemiesArray);
-}, 2000);
-
-// d3.interval(function () {
-//   updatePosition(enemiesArray);
-// }, 0, 100);
-// updatePosition(enemiesArray);
-// d3.interval(function() {
-//   updatePosition(enemiesArray);
-// }, 100);
-
-var Player = function () {
-  this.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
-  this.fill = '#ff6600';
-  this.x = gameOptions.width * 0.5;
-  this.y = gameOptions.height * 0.5;
-  this.angle = 0;
-  this.r = 5;
-
-};
-
-
-
+}, 1000);
 
 
 // d3.select("body").select(".board").selectAll("div")
