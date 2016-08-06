d79cc3cd16dbbfa7bb3eff8afad8ebb1a0aa1641
Truong-Julie <gamerlazer@gmail.com>
2016-08-06 11:07:49 -0700

    Implement drag feature of player

--- client/watchout.js
+++ client/watchout.js
@@ -46,7 +46,6 @@ var Enemy = function(id) {
 
 var Player = function () {
   this.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
-  this.fill = 'orange';
   this.x = gameOptions.width * 0.5;
   this.y = gameOptions.height * 0.5;
   this.angle = 0;
@@ -58,16 +57,30 @@ var Player = function () {
 /****************** ADD PLAYER TO THE BOARD ***********************/
 var mouseArray = [new Player ()];
 // console.log(mouse);
+var drag = d3.behavior.drag()
+            .on("drag", function(d) {
+              d.x += d3.event.dx;
+              d.y += d3.event.dy;
+              d3.select(this).attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+              });
+            });
 
 d3.selectAll(".mouse").selectAll("circle")
   .data(mouseArray)
   .enter()
   .append("circle")
-  .attr('cx', function(d) { return d.x; })
-  .attr('cy', function(d) { return d.y; });
+  // .attr('d', function(d) { return d.path; })
+  // .attr('view-box', function(d) { return d.x+d.y 0 0});
+  // .attr('cx', function(d) { return d.x; })
+  // .attr('cy', function(d) { return d.y; })
+  // .attr("transform", "translate ( 350, 400 )" )
+  .attr("transform", function(d) { return "translate (" + [d.x, d.y] + ")"; })
+  .call(drag);
 // ;
 
-  
+// DRAG FUNCTION
+// d3.select(".mouse").select("circle").call()
 
 
 /****************** ADD ENEMIES TO THE BOARD **********************/
